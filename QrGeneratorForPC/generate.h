ce5230c9a1fae16e257b2120f8c4261ff085757b
Zentor66 <155506765+Zentor66@users.noreply.github.com>
2024-12-13 10:59:53 +0300

    Add files via upload

---
+++ QrGeneratorForPC/generate.h
@@ -0,0 +1,17 @@
+#ifndef GENERATE_H
+#define GENERATE_H
+#include <QString>
+#include <QMap>
+
+class Generate
+{
+public:
+    void set_pcname_n_series(const QString& pc_name, const QString& pc_series);
+    bool gen_qr();
+    Generate();
+
+private:
+    QString pc_name;
+};
+
+#endif // GENERATE_H
